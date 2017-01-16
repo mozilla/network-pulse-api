c19b37bf05bceddaa2eac0d5d132d90f07f1b404
Pomax <pomax@nihongoresources.com>
2017-01-16 14:22:58 -0800

    This adds a ./login route that can take ?original_url=....

--- pulseapi/users/urls.py
+++ pulseapi/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login$', views.start_auth, name='login'),
     url(r'^logout', views.force_logout, name='logout'),
     url(r'^oauth2callback', views.callback, name='oauthcallback'),
     url(r'^nonce', views.nonce, name="get a new nonce value"),
