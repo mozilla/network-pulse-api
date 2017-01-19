8bdad6c4a000bbb084701616f83ec4b2ff9e6b60
Alan Mooiman <amooiman@gmail.com>
2017-01-19 17:29:35 -0500

    Comment on reasoning behind page size

--- pulseapi/entries/views.py
+++ pulseapi/entries/views.py
@@ -51,6 +51,7 @@ class EntriesPagination(PageNumberPagination):
     """
     Add support for pagination and custom page size
     """
+    # page size decided in https://github.com/mozilla/network-pulse-api/issues/39
     page_size = 48
     page_size_query_param = 'page_size'
     max_page_size = 1000
