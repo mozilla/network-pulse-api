135969a801c9b2598ff644da94f891c437306f18
Alan Mooiman <amooiman@gmail.com>
2017-01-19 15:38:18 -0500

    User favorites route

--- pulseapi/entries/urls.py
+++ pulseapi/entries/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import url
 from pulseapi.entries.views import (
     EntriesListView,
     EntryView,
-    toggle_bookmark
+    toggle_bookmark,
+    FavoriteEntries,
 )
 
 urlpatterns = [
     url('^$', EntriesListView.as_view(), name='entries-list'),
+    url('favorites/', FavoriteEntries.as_view(), name='user-favorites'),
     url(r'^(?P<entryid>[0-9]+)/bookmark/?', toggle_bookmark, name='bookmark'),
     url(r'^(?P<pk>[0-9]+)/', EntryView.as_view(), name='entry'),
 ]
