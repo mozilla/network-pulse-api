45ec69716a6662bb4ba09f3871d175b9a9f68b1f
Mavis Ou <mmmavis@users.noreply.github.com>
2017-06-23 14:53:51 -0700

    wip, there are some errors need fixing

--- pulseapi/issues/tests.py
+++ pulseapi/issues/tests.py
@@ -1,6 +1,6 @@
 import json
 from django.test import TestCase
-
+from pulseapi.issues.models import Issue
 
 class TestIssues(TestCase):
 
@@ -9,3 +9,9 @@ class TestIssues(TestCase):
         issuesJson = json.loads(str(issues.content, 'utf-8'))
         self.assertEqual(issues.status_code, 200)
         self.assertEqual(len(issuesJson), 5)
+
+        issue_from_api = issuesJson[0]
+        self.assertEqual('description' in issue_from_api, True)
+
+        issue_from_db = Issue.objects.get(name=issue_from_api['name'])
+        self.assertEqual(issue_from_db['description'], issue_from_api['description'])
