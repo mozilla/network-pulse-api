31e1d37450e79f658cf84668ed9f0fae9c8d3d27
Pomax <pomax@nihongoresources.com>
2017-07-21 15:56:05 -0700

    test fix

--- pulseapi/helptypes/tests.py
+++ pulseapi/helptypes/tests.py
@@ -1,17 +1,17 @@
 import json
 from django.test import TestCase
-from .models import Issue
+from .models import HelpType
 
 class TestIssues(TestCase):
 
     def test_check_for_help_types(self):
-        issues = self.client.get('/api/pulse/helptypes/')
-        issuesJson = json.loads(str(issues.content, 'utf-8'))
-        self.assertEqual(issues.status_code, 200)
-        self.assertEqual(len(issuesJson), 12)
+        help_types = self.client.get('/helptypes/')
+        help_type_json = json.loads(str(help_types.content, 'utf-8'))
+        self.assertEqual(help_types.status_code, 200)
+        self.assertEqual(len(help_type_json), 13)
 
-        help_type_from_api = issuesJson[0]
+        help_type_from_api = help_type_json[0]
         self.assertEqual('description' in help_type_from_api, True)
 
-        issue_from_db = Issue.objects.get(name=help_type_from_api['name'])
-        self.assertEqual(issue_from_db.description, help_type_from_api['description'])
+        help_type_from_db = HelpType.objects.get(name=help_type_from_api['name'])
+        self.assertEqual(help_type_from_db.description, help_type_from_api['description'])
