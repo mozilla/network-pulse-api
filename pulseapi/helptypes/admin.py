edf634d41a39fc7927615df06d27a4a430210706
Mike Kamermans <pomax@nihongoresources.com>
2017-07-24 15:10:42 -0700

    removed copy-pasta

--- pulseapi/helptypes/admin.py
+++ pulseapi/helptypes/admin.py
@@ -1,7 +1,3 @@
-"""
-Uncomment these lines to enable admin of Issues.
-But you probably don't need to since they're fixed data.
-"""
 from django.contrib import admin
 
 from .models import HelpType
