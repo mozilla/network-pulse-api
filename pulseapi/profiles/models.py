1aa78e85a4c3d8a61bc757bb9c952e83a9a846a3
Pomax <pomax@nihongoresources.com>
2017-08-14 15:31:07 -0700

    rebase

--- pulseapi/profiles/models.py
+++ pulseapi/profiles/models.py
@@ -1,5 +1,46 @@
 from django.db import models
 
+def entry_thumbnail_path(instance, filename):
+    return 'images/user-avatars/{timestamp}{ext}'.format(
+        timestamp=str(timezone.now()),
+        ext=os.path.splitext(filename)[1]
+    )
+
+
+class Location(models.Model):
+    """
+    THIS COMES WITH ASSUMPTION-PROBLEMS:
+    - A country might have two or more cities with the same name, but
+      in different administative regions of that country.
+    - Some cities lie in to countries. I know, that's crazy, but some do.
+
+    Making this do the right thing is going to take more work than any
+    initial model implementation can achieve.
+    """
+    city = models.CharField(max_len=500, blank=False, null=True)
+    country  = models.CharField(max_len=500, blank=False, null=True)
+
+
+class SocialUrl(models.Model):
+    url = models.URLField()
+
+    service = models.CharField(
+        max_len=500,
+        blank=False,
+        null=True
+    )
+
+    def get_service_name(self):
+        """
+        This function can "guess" at what the service name for this
+        URL is based on the domain found in the url. This lets us
+        say things like "this is twitter" or "this is facebook" or
+        "a blog", or it might be prespecified by the user, or we
+        will be unable to guess, in which case the result is False
+        """
+        if self.service is not Null:
+            return self.service
+        return False
 
 class UserProfile(models.Model):
     """
@@ -7,12 +48,26 @@ class UserProfile(models.Model):
     such as real name, social media information,
     bookmarks on the site, etc.
     """
-
     user = models.ForeignKey(
         'users.EmailUser',
         on_delete=models.SET_NULL,
         related_name='profile',
         null=True
+    )
+
+    # A tweet-style user bio
+    user_blurp = models.CharField(
+        max_len=140,
+        blank=True,
+        default=''
+    )
+
+    # A longer user bio that can be exposed when clicking through
+    # to a user's profile because you care to know what they're about.
+    user_bio = models.CharField(
+        max_len=1000,
+        blank=True,
+        default=''
     )
 
     # "user X bookmarked entry Y" is a many to many relation,
@@ -23,6 +78,83 @@ class UserProfile(models.Model):
         'entries.Entry',
         through='profiles.UserBookmarks'
     )
+
+    # The name field is an alternative name to be used if the
+    # associated user(s) don't want to expose their login-indicated
+    # name instead.
+    #
+    # Examples of this are nicknames, pseudonyms, and org names
+    alternative_name = models.CharField(
+        max_len=500,
+        blank=False,
+        null=True
+    )
+
+    # Accessing the Profile-indicated name needs various stages
+    # of fallback.
+    def get_name(self):
+        if self.alternative_name is not Null:
+            return self.alternative_name
+        if self.user is not Null:
+            return self.user.name
+        return ''
+
+    get_name.short_description = 'name'
+
+
+    # This flag marks whether or not this profile applies to
+    # "A human being", or a group of people (be that a club, org,
+    # institution, etc. etc.)
+    is_group = models.BooleanField(
+        default=False
+    )
+
+    # We want to capture the user or group's location, but we don't
+    # want to do so in too fine a detail, so right now we mostly
+    # care about city, and country that city is in.
+    location = models.ForeignKey(Location)
+
+    # Thumbnail image for this user; their "avatar" even though we
+    # do not have anything on the site right now where avatars
+    # come into play.
+    thumbnail = models.ImageField(
+        max_length=2048,
+        upload_to=entry_thumbnail_path,
+        blank=True
+    )
+
+    def thumbnail_image_tag(self):
+        if not self.thumbnail:
+            return format_html('<span>No image to preview</span>')
+
+        media_url = settings.MEDIA_URL
+
+        if settings.USE_S3:
+            media_url = 'https://{domain}/{bucket}/'.format(
+                domain=settings.AWS_S3_CUSTOM_DOMAIN,
+                bucket=settings.AWS_LOCATION
+            )
+
+        html = '<img src="{media_url}{src}" style="width:25%">'.format(
+            media_url=media_url,
+            src=self.thumbnail
+        )
+
+        return format_html(html)
+
+    thumbnail_image_tag.short_description = 'Thumbnail preview'
+
+    # A UserProfile can have any number of associated social urls
+    social_urls = models.ManyToManyField(
+        SocialUrl,
+        related_name='profile',
+        blank=False,
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    # Which issues does this user care about/are they involved in?
+    issues = models.ManyToManyField('issues.Issue')
 
     def __str__(self):
         return 'profile for {}'.format(self.user.email)
