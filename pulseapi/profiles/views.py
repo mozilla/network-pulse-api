c615a8f6fc3ba8779ee67c8c33917d583cfb2785
Gideon Thomas <gideon@mozillafoundation.org>
2017-10-04 10:46:05 -0400

    fix(profiles): use correct lookup field for own profile
    Fixes #224 - Profile endpoint erroring out

--- pulseapi/profiles/views.py
+++ pulseapi/profiles/views.py
@@ -26,7 +26,7 @@ class UserProfilePublicAPIView(RetrieveAPIView):
 class UserProfilePublicSelfAPIView(UserProfilePublicAPIView):
     def get_object(self):
         user = self.request.user
-        return get_object_or_404(self.queryset, user=user)
+        return get_object_or_404(self.queryset, related_user=user)
 
 
 class UserProfileAPIView(RetrieveUpdateAPIView):
@@ -38,7 +38,7 @@ class UserProfileAPIView(RetrieveUpdateAPIView):
 
     def get_object(self):
         user = self.request.user
-        return get_object_or_404(UserProfile, user=user)
+        return get_object_or_404(UserProfile, related_user=user)
 
     @detail_route(methods=['put'])
     def put(self, request, *args, **kwargs):
