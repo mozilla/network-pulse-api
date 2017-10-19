2d249af49192a8bf6790f053e2fff03d88654af4
Mike Kamermans <pomax@nihongoresources.com>
2017-10-19 11:18:28 -0700

    Update views.py

--- pulseapi/creators/views.py
+++ pulseapi/creators/views.py
@@ -25,7 +25,7 @@ class FilterCreatorNameBackend(filters.BaseFilterBackend):
         if not search_term:
             return queryset
 
-        own_name = Q(name__startswith=search_term)
+        own_name = Q(name__istartswith=search_term)
         profile_custom = Q(profile__custom_name__istartswith=search_term)
         profile_name = Q(profile__related_user__name__istartswith=search_term)
 
