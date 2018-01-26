0a2c1e35522d70464bba92facb03d83c56a4a954
Lucie Daeye <lucied@mozillafoundation.org>
2018-01-26 11:14:17 +0100

    refacto + fixes

--- pulseapi/utility/syndication.py
+++ pulseapi/utility/syndication.py
@@ -11,25 +11,18 @@ from pulseapi.entries.models import Entry
 # The creator(s) name can be found in the `OrderedCreatorRecord` class from the `creators` models.
 def get_entry_creators(entry):
     # Since `creators` is an optional field and can be empty, we return the publisher name instead.
-    if not entry.related_creators.all():
-        return entry.published_by.name
-    else:
+    if entry.related_creators.count() >= 1:
         return ', '.join(
             creator_record.creator.creator_name
             for creator_record
             in entry.related_creators.all()
         )
+    else:
+        return entry.published_by.name
 
 
-# RSS feed for latest entries
-class RSSFeedLatestFromPulse(Feed):
-    title = "Latest from Mozilla Pulse"
-    link = "{frontend_url}/latest".format(frontend_url=settings.PULSE_FRONTEND_HOSTNAME)
-    description = "Subscribe to get the latest entries from Mozilla Pulse."
-
-    def items(self):
-        return Entry.objects.order_by('-created')
-
+# Generic class for RSS feeds
+class RSSFeedFromPulse(Feed):
     def item_author_name(self, entry):
         return get_entry_creators(entry)
 
@@ -43,12 +36,12 @@ class RSSFeedLatestFromPulse(Feed):
         if entry.thumbnail:
             if settings.USE_S3:
                 return entry.thumbnail.url
-            # Provide an absolute URL
+            # Provide an absolute URL for local dev purposes
             else:
                 return settings.ALLOWED_HOSTS[0] + ':8000' + entry.thumbnail.url
 
     def item_enclosure_length(self, entry):
-        return ""
+        return ''
 
     def item_enclosure_mime_type(self, entry):
         return 'image/jpeg'
@@ -57,26 +50,24 @@ class RSSFeedLatestFromPulse(Feed):
         return entry.frontend_entry_url()
 
 
+# RSS feed for latest entries
+class RSSFeedLatestFromPulse(RSSFeedFromPulse):
+    title = 'Latest from Mozilla Pulse'
+    link = '{frontend_url}/latest'.format(frontend_url=settings.PULSE_FRONTEND_HOSTNAME)
+    description = 'Subscribe to get the latest entries from Mozilla Pulse.'
+
+    def items(self):
+        return Entry.objects.order_by('-created')
+
+
 # RSS feed for featured entries
-class RSSFeedFeaturedFromPulse(Feed):
-    title = "Latest from Mozilla Pulse"
-    link = "{frontend_url}/featured".format(frontend_url=settings.PULSE_FRONTEND_HOSTNAME)
-    description = "Subscribe to get the latest featured entries from Mozilla Pulse."
+class RSSFeedFeaturedFromPulse(RSSFeedFromPulse):
+    title = 'Latest from Mozilla Pulse'
+    link = '{frontend_url}/featured'.format(frontend_url=settings.PULSE_FRONTEND_HOSTNAME)
+    description = 'Subscribe to get the latest featured entries from Mozilla Pulse.'
 
     def items(self):
         return Entry.objects.filter(featured=True).order_by('-created')
-
-    def item_author_name(self, entry):
-        return get_entry_creators(entry)
-
-    def item_title(self, entry):
-        return entry.title
-
-    def item_description(self, entry):
-        return entry.description
-
-    def item_link(self, entry):
-        return entry.frontend_entry_url()
 
 
 # Atom feed for latest entries
