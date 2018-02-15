543c5946f9296cf00d35837ebaab4b9f1db038e5
Lucie Daeye <lucied@mozillafoundation.org>
2018-02-15 17:25:56 +0100

    small fixes + fix warning for python version

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: sh release.sh
+release: python manage.py migrate
 web: python /app/generate_client_secrets.py && gunicorn pulseapi.wsgi
--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Make Django migrations, just in case...
-python manage.py makemigrations
-
-# Then migrate up.
-python manage.py migrate
