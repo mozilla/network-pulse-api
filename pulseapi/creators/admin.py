02d037990892017f12eb73224a2adacffa98b990
Gideon Thomas <gideon@mozillafoundation.org>
2018-03-01 17:21:30 -0500

    Allow choosing creator for a profile in admin

--- pulseapi/creators/admin.py
+++ pulseapi/creators/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Creator
 
-admin.site.register(Creator)
+
+class CreatorAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+        'profile__custom_name',
+        'profile__related_user__name',
+    )
+
+admin.site.register(Creator, CreatorAdmin)
--- pulseapi/profiles/signals.py
+++ pulseapi/profiles/signals.py
@@ -10,5 +10,14 @@ def create_creator_for_profile(sender, **kwargs):
     """
     Automatically create a corresponding Creator instance for every profile that is created.
     """
+    instance = kwargs.get('instance')
+    # This will check to see if a creator was passed in and use that
+    # to bind to the current profile instead of creating a new one
+    creator = instance._creator if hasattr(instance, '_creator') else None
+
     if kwargs.get('created', False) and not kwargs.get('raw', False):
-        Creator.objects.get_or_create(profile=kwargs.get('instance'))
+        if creator is not None:
+            creator.profile = instance
+            creator.save()
+        else:
+            Creator.objects.get_or_create(profile=kwargs.get('instance'))
