9a4ec35158dc3b1af78a9f78931687f74a415005
Gideon Thomas <gideon@mozillafoundation.org>
2018-03-01 19:04:58 -0500

    Fix warning message

--- pulseapi/profiles/static/admin/js/creator-profile-rebind-protection.js
+++ pulseapi/profiles/static/admin/js/creator-profile-rebind-protection.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
   selector.addEventListener("change", function(e) {
     result = window.confirm(
       "Are you sure you want to change this binding?\n\n" +
-      "WARNING: If you set this to a creator that is already associated with another profile, that profile will lose its association to the selected creator and all entries that referenced that creator will now link to this profile."
+      "WARNING: This change CANNOT be undone. If you set this to a creator that is already associated with another profile, that profile will lose its association to the selected creator and all entries that referenced that creator will now link to this profile."
     );
 
     if (!result) {
