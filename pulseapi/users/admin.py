c2caedbbd6eb56c7c9f72f69c2460b3155dc8abe
Gideon Thomas <r.gideonthomas@gmail.com>
2018-03-13 15:08:03 -0400

    Use customized django admin view for users (#325)

--- pulseapi/users/admin.py
+++ pulseapi/users/admin.py
@@ -2,6 +2,7 @@
 Admin setings for EmailUser app
 """
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 from django.utils.html import format_html
 
@@ -13,29 +14,44 @@ from pulseapi.utility.get_admin_url import get_admin_url
 from .admin_group_editing import GroupAdmin
 
 
-class EmailUserAdmin(admin.ModelAdmin):
+class EmailUserAdmin(UserAdmin):
     """
     Show a list of entries a user has submitted in the EmailUser Admin app
     """
-    fields = (
-        'password',
-        'last_login',
-        'email',
-        'name',
-        'is_staff',
-        'is_superuser',
-        'user_profile',
-        'entries',
+    fieldsets = (
+        (None, {
+            'fields': (
+                'user_password',
+                'last_login',
+                'email',
+                'name',
+                'is_staff',
+                'is_superuser',
+                'user_profile',
+                'entries',
+            )
+        }),
     )
 
     readonly_fields = (
         'entries',
         'user_profile',
         'name',
+        'user_password',
     )
 
     list_display = ('name', 'email', 'profile',)
+    list_filter = ('is_staff', 'is_superuser', 'groups',)
     search_fields = ('name', 'email',)
+    ordering = ('name',)
+
+    def user_password(self, instance):
+        return format_html(
+            '<div>Raw passwords are not stored, so there is no '
+            'way to see this user\'s password, but you can change the password '
+            'using <a href="../password/">this form</a>.</div>'
+        )
+    user_password.short_description = 'Password'
 
     def entries(self, instance):
         entries = Entry.objects.filter(published_by=instance)
