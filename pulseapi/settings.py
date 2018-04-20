bc64afd40ef8d67c016ad15c92c6761251de6302
Anubhav-Bhargava <anubhavbhargava007@gmail.com>
2018-04-20 23:14:40 +0530

    Added python version check (#346)

--- pulseapi/settings.py
+++ pulseapi/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import sys
 import dj_database_url
 import environ
+
+if sys.version_info < (3, 6):
+    raise ValueError("Please upgrade to Python 3.6 or later")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
