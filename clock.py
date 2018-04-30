0681f2aae402623a513f0af69e7e0604b6b1326b
Chris DeCairos <cade@mozillafoundation.org>
2018-04-30 13:59:35 -0400

    we can only run twice an hour

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=15)
+@sched.scheduled_job('interval', minutes=30)
 def timed_job():
     from pulseapi.utility import contributions_data
     t = threading.Thread(
