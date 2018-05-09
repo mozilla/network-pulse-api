644b550d6e8b420af029f3770ba59cdde3ecbbcf
Christopher DeCairos <cade@mozillafoundation.org>
2018-05-09 11:58:22 -0400

    Update load_fake_data.py

--- pulseapi/utility/management/commands/load_fake_data.py
+++ pulseapi/utility/management/commands/load_fake_data.py
@@ -36,7 +36,7 @@ class Command(BaseCommand):
         )
 
         parser.add_argument(
-            '--fellowsCount',
+            '--fellows-count',
             action='store',
             type=int,
             default=3,
