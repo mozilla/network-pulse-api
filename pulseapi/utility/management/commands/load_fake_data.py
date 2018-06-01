ff9d9b7438dfdf21cf2014a876dee5889d7cca1e
Lucie Daeye <lucied@mozillafoundation.org>
2018-06-01 16:03:27 +0200

    Add fellow creation back

--- pulseapi/utility/management/commands/load_fake_data.py
+++ pulseapi/utility/management/commands/load_fake_data.py
@@ -1,7 +1,7 @@
 """
 Populate the database with fake data. Used for Heroku's review apps and local development.
 """
-from itertools import chain, combinations
+from itertools import chain, combinations, product
 
 import factory
 from random import randint, sample
@@ -14,7 +14,8 @@ from pulseapi.entries.models import Entry
 # Factories
 from pulseapi.creators.factory import EntryCreatorFactory
 from pulseapi.entries.factory import BasicEntryFactory, GetInvolvedEntryFactory
-from pulseapi.profiles.factory import UserBookmarksFactory
+from pulseapi.profiles.factory import UserBookmarksFactory, ExtendedUserProfileFactory
+from pulseapi.profiles.models import ProgramYear, ProgramType, ProfileType
 from pulseapi.tags.factory import TagFactory
 from pulseapi.users.factory import BasicEmailUserFactory, MozillaEmailUserFactory
 
@@ -22,7 +23,7 @@ from pulseapi.users.factory import BasicEmailUserFactory, MozillaEmailUserFactor
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
 # Create a list of dictionaries containing every factory params permutation possible. ex: [{'group': True},
@@ -37,6 +38,24 @@ def generate_fake_data(factory_model, count):
     for kwargs in generate_variations(factory_model):
         for i in range(count):
             factory_model.create(**kwargs)
+
+
+def generate_fellows(count):
+    fellows_profile_type = ProfileType.objects.get(value='fellow')
+    program_years = ProgramYear.objects.all()
+    program_types = ProgramType.objects.all()
+
+    for (program_year, program_type) in list(product(program_years, program_types)):
+        for i in range(count):
+            ext_profile = ExtendedUserProfileFactory(
+                profile_type=fellows_profile_type,
+                program_year=program_year,
+                program_type=program_type
+            )
+            BasicEmailUserFactory.create(
+                active=True,
+                profile=ext_profile
+            )
 
 
 class Command(BaseCommand):
@@ -102,34 +121,35 @@ class Command(BaseCommand):
         else:
             seed = randint(0, 5000000)
 
-        self.stdout.write('Seeding Faker with {}'. format(seed))
+        print('Seeding Faker with {}'. format(seed))
 
         faker = factory.faker.Faker._get_faker(locale='en-US')
         faker.random.seed(seed)
 
-        self.stdout.write('Creating tags')
+        print('Creating tags')
         [TagFactory.create() for i in range(options['tags_count'])]
 
-        self.stdout.write('Creating users')
+        print('Creating users')
         generate_fake_data(BasicEmailUserFactory, options['users_count'])
         generate_fake_data(MozillaEmailUserFactory, options['users_count'])
 
-        self.stdout.write('Creating pulse entries')
+        print('Creating pulse entries')
         generate_fake_data(BasicEntryFactory, options['entries_count'])
         generate_fake_data(GetInvolvedEntryFactory, options['entries_count'])
 
-        self.stdout.write('Creating fellows')
+        print('Creating fellows')
+        generate_fellows(options['fellows_count'])
 
         # Select random published entries and bookmark them for 1 to 10 users
-        self.stdout.write('Creating bookmarks')
+        print('Creating bookmarks')
         approved_entries = Entry.objects.public().with_related()
         for e in sample(list(approved_entries), k=len(approved_entries) // 2):
             [UserBookmarksFactory.create(entry=e) for i in range(randint(1, 10))]
 
         # Select random entries and link them to 1 to 5 creators
-        self.stdout.write('Linking random profiles as creators with random entries')
+        print('Linking random profiles as creators with random entries')
         all_entries = Entry.objects.all()
         for e in sample(list(all_entries), k=len(all_entries) // 2):
             [EntryCreatorFactory.create(entry=e) for i in range(randint(1, 5))]
 
-        self.stdout.write(self.style.SUCCESS('Done!'))
+        print(self.style.SUCCESS('Done!'))
