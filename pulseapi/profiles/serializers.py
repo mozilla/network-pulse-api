8f25c2603637b27063c192f6410eff238a2d44cd
Gideon Thomas <gideon@mozillafoundation.org>
2018-07-03 17:15:36 -0400

    Allow ordering of profile entries via querystring

--- pulseapi/profiles/serializers.py
+++ pulseapi/profiles/serializers.py
@@ -215,6 +215,18 @@ class UserProfileEntriesSerializer(serializers.Serializer):
     returning the number of entries (created, published, and favorited) associated
     with the profile.
     """
+    @staticmethod
+    def get_ordered_queryset(queryset, ordering_param=None, prefix=False):
+        if not ordering_param:
+            return queryset
+
+        if prefix:
+            if ordering_param[0] is '-':
+                ordering_param = '-' + prefix + ordering_param[1:]
+            else:
+                ordering_param = prefix + ordering_param
+
+        return queryset.order_by(ordering_param)
 
     def to_representation(self, instance):
         data = {}
@@ -223,6 +235,9 @@ class UserProfileEntriesSerializer(serializers.Serializer):
         include_created = context.get('created', False)
         include_published = context.get('published', False)
         include_favorited = context.get('favorited', False)
+        created_ordering = context.get('created_ordering', False)
+        published_ordering = context.get('published_ordering', False)
+        favorited_ordering = context.get('favorited_ordering', False)
         EntrySerializerClass = context.get('EntrySerializerClass', EntryWithCreatorsBaseSerializer)
 
         # If none of the filter options are provided, only return the count of
@@ -243,11 +258,15 @@ class UserProfileEntriesSerializer(serializers.Serializer):
         )
 
         if include_created:
-            entry_creators = (
-                EntryCreator.objects
-                .prefetch_related(Prefetch('entry', queryset=entry_queryset))
-                .filter(profile=instance)
-                .filter(entry__in=Entry.objects.public())
+            entry_creators = UserProfileEntriesSerializer.get_ordered_queryset(
+                queryset=(
+                    EntryCreator.objects
+                    .prefetch_related(Prefetch('entry', queryset=entry_queryset))
+                    .filter(profile=instance)
+                    .filter(entry__in=Entry.objects.public())
+                ),
+                ordering_param=created_ordering,
+                prefix='entry__',
             )
             data['created'] = [
                 EntrySerializerClass(
@@ -258,7 +277,14 @@ class UserProfileEntriesSerializer(serializers.Serializer):
             ]
 
         if include_published:
-            entries = entry_queryset.public().filter(published_by=instance.user) if instance.user else []
+            if instance.user:
+                entries = UserProfileEntriesSerializer.get_ordered_queryset(
+                    queryset=entry_queryset.public().filter(published_by=instance.user),
+                    ordering_param=published_ordering,
+                )
+            else:
+                entries = []
+
             data['published'] = EntrySerializerClass(
                 entries,
                 context={'user': user},
@@ -266,12 +292,17 @@ class UserProfileEntriesSerializer(serializers.Serializer):
             ).data
 
         if include_favorited:
-            user_bookmarks = UserBookmarks.objects.filter(profile=instance)
+            user_bookmarks = UserProfileEntriesSerializer.get_ordered_queryset(
+                queryset=(
+                    UserBookmarks.objects.filter(profile=instance)
+                    .filter(entry__in=Entry.objects.public())
+                    .prefetch_related(Prefetch('entry', queryset=entry_queryset))
+                ),
+                ordering_param=favorited_ordering,
+                prefix='entry__',
+            )
             data['favorited'] = [
-                EntrySerializerClass(bookmark.entry, context={'user': user}).data for bookmark in
-                user_bookmarks.prefetch_related(
-                    Prefetch('entry', queryset=entry_queryset)
-                ).filter(entry__in=Entry.objects.public())
+                EntrySerializerClass(bookmark.entry, context={'user': user}).data for bookmark in user_bookmarks
             ]
 
         return data
