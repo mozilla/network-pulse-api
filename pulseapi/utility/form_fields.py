dd73ba101b2f44f1ed114c25a465cbbbe32e91bc
Chris DeCairos <cade@mozillafoundation.org>
2018-08-10 15:54:20 -0400

    Add custom model and form fields for temporary org profile access feature.

---
+++ pulseapi/utility/form_fields.py
@@ -0,0 +1,28 @@
+import uuid
+from django.forms import fields, widgets
+
+
+class TemporaryCheckboxInput(widgets.CheckboxInput):
+
+    template_name = 'widgets/temporary_checkbox.html'
+
+
+    def value_from_datadict(self, data, files, name):
+        value = data.get(name)
+        if value is None:
+            return value
+        try:
+            value = uuid.UUID(value)
+        except ValueError:
+            value = uuid.uuid4()
+        return value
+
+
+class TemporaryField(fields.UUIDField):
+
+    widget = TemporaryCheckboxInput
+
+
+    def __init__(self, *args, **kwargs):
+        kwargs['required'] = False
+        super().__init__(*args, **kwargs)
---
+++ pulseapi/utility/model_fields.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class TemporaryField(models.UUIDField):
+
+    description = 'A unique identifier for a temporary instance'
+
+
+    def __init__(self, *args, **kwargs):
+        kwargs['verbose_name'] = None
+        kwargs['null'] = True
+        kwargs['unique'] = True
+        super().__init__(*args, **kwargs)
+
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs["verbose_name"]
+        del kwargs["null"]
+        del kwargs["unique"]
+        return name, path, args, kwargs
