465c9cd3291fbba441d8164f4e1dcd78546c70a5
Lucie Daeye <lucied@mozillafoundation.org>
2018-10-10 15:42:39 +0200

    config files

---
+++ shell.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixpkgs-18.09-darwin.tar.gz) {} }:
+
+let pipenv = pkgs.pipenv.overrideAttrs (attrs: {name = "pipenv-2018-10-9"; src = pkgs.python3Packages.fetchPypi { pname = "pipenv"; version = "2018.10.9"; sha256 = "0b0safavjxq6malmv44acmgds21m2sp1wqa7gs0qz621v6gcgq4j";};}); in
+
+  pkgs.mkShell {
+    buildInputs = [ pkgs.python36 pipenv];
+}
