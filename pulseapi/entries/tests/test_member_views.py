4dd33f73edb05ff679241640539c9b61b7bb43c3
Pomax <pomax@nihongoresources.com>
2020-02-26 11:09:32 -0800

    functional parity with DRF 3.6.3

--- pulseapi/entries/tests/test_member_views.py
+++ pulseapi/entries/tests/test_member_views.py
@@ -147,4 +147,4 @@ class TestEntryAPIJSONView(PulseMemberTestCase):
         self.assertEqual(response.status_code, 200)
         response_data = json.loads(str(response.content, 'utf-8'))
         count = response_data['count']
-        self.assertEqual(count, 3)
+        self.assertEqual(count, 1)
--- pulseapi/entries/views.py
+++ pulseapi/entries/views.py
@@ -12,7 +12,6 @@ from django.core.files.base import ContentFile
 from django.conf import settings
 from django.db import models
 from django.db.models import Q
-from django.utils import six
 
 from functools import reduce
 
@@ -308,23 +307,32 @@ class ModerationStateView(ListAPIView):
 
 # see https://stackoverflow.com/questions/60326973
 class SearchWithNormalTagFiltering(filters.SearchFilter):
-    def filter_queryset(self, request, queryset, view):
-        """
-        override https://github.com/encode/django-rest-framework/blob/3.6.3/rest_framework/filters.py#L129-L155
-        """
-        search_fields = getattr(view, 'search_fields', None)
-        search_terms = self.get_search_terms(request)
+    """
+    This is a custom search filter that allows the same kind of
+    matching that DRF v3.6.3 allowed wrt many to many relations,
+    where multiple terms have to all match, but do _not_ need
+    to match against single m2m relations, so a ?search=a,b
+    will match an entry with two tags a and b, but not with
+    single tag a or tag b.
+    """
+    def required_m2m_optimization(self, view):
+        return getattr(view, 'use_m2m_optimization', True)
 
-        if not search_fields or not search_terms:
-            return queryset
+    def get_search_fields(self, view, request):
+        # For DRF versions >=3.9.2 remove this method,
+        # as it already has get_search_fields built in.
+        return getattr(view, 'search_fields', None)
 
-        orm_lookups = [
-            self.construct_search(six.text_type(search_field))
-            for search_field in search_fields
-        ]
+    def chained_queryset_filter(self, queryset, search_terms, orm_lookups):
+        for search_term in search_terms:
+            queries = [
+                models.Q(**{orm_lookup: search_term})
+                for orm_lookup in orm_lookups
+            ]
+            queryset = queryset.filter(reduce(operator.or_, queries))
+        return queryset
 
-        # Adopted from a future version of DRF:
-        base = queryset
+    def optimized_queryset_filter(self, queryset, search_terms, orm_lookups):
         conditions = []
         for search_term in search_terms:
             queries = [
@@ -333,8 +341,25 @@ class SearchWithNormalTagFiltering(filters.SearchFilter):
             ]
             conditions.append(reduce(operator.or_, queries))
 
-        # This is the only line we add:
-        queryset = queryset.filter(reduce(operator.or_, conditions))
+        return queryset.filter(reduce(operator.and_, conditions))
+
+    def filter_queryset(self, request, queryset, view):
+        search_fields = self.get_search_fields(view, request)
+        search_terms = self.get_search_terms(request)
+
+        if not search_fields or not search_terms:
+            return queryset
+
+        orm_lookups = [
+            self.construct_search(str(search_field))
+            for search_field in search_fields
+        ]
+
+        base = queryset
+        if self.required_m2m_optimization(view):
+            queryset = self.optimized_queryset_filter(queryset, search_terms, orm_lookups)
+        else:
+            queryset = self.chained_queryset_filter(queryset, search_terms, orm_lookups)
 
         if self.must_call_distinct(queryset, search_fields):
             # Filtering against a many-to-many field requires us to
@@ -390,6 +415,11 @@ class EntriesListView(ListCreateAPIView):
         'interest',
         'tags__name',
     )
+
+    # Forces DRF to use pre-3.6.4 matching for many-to-many relations, so that
+    # searching for multiple terms will find only entry that match all terms,
+    # but many-to-many will resolve on the m2m set, not "one relation in the set".
+    use_m2m_optimization = False
 
     parser_classes = (
         JSONParser,
