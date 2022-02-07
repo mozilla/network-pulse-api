7e21762afb7f6803b9531227b0efce1a63f2baad
Pomax <pomax@nihongoresources.com>
2022-02-07 10:26:44 -0800

    shebang

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+
+# Ensure that google auth works (sigh)
 python generate_client_secrets.py
+
+# Make sure the Django database schemas are up to date
 python manage.py migrate --no-input
