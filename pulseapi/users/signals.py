db9be72cd447708d1d6523612028d559222b66f5
Daniel Miranda <manieldiranda@gmail.com>
2022-04-06 13:47:32 -0700

    removed unneccesary import

--- pulseapi/users/signals.py
+++ pulseapi/users/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete
 
 from pulseapi.users.models import EmailUser
 from pulseapi.profiles.models import UserProfile
