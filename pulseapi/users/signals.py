c8a7b4f667eccaf762c08f52a5971c38846d767c
Daniel Miranda <manieldiranda@gmail.com>
2022-04-06 13:32:47 -0700

    updated signals to avoid orphan profiles

--- pulseapi/users/signals.py
+++ pulseapi/users/signals.py
@@ -5,15 +5,6 @@ from pulseapi.users.models import EmailUser
 from pulseapi.profiles.models import UserProfile
 
 
-@receiver(post_save, sender=EmailUser)
-def create_profile_for_user(sender, **kwargs):
-    if kwargs.get('created', False):
-        UserProfile.objects.get_or_create(
-            related_user=kwargs.get("instance"),
-            is_active=True,
-        )
-
-
 @receiver(post_delete, sender=EmailUser)
 def delete_profile_for_user(sender, **kwargs):
     related_profile_id = kwargs['instance'].profile_id
