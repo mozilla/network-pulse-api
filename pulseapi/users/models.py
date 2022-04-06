54e20be9aa7183e8f2e1f08b770ce796e989d571
Daniel Miranda <manieldiranda@gmail.com>
2022-04-06 12:07:56 -0700

    post_delete signal to delete profile working

--- pulseapi/users/models.py
+++ pulseapi/users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin
 )
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
 
 from pulseapi.profiles.models import UserProfile
 
@@ -94,3 +96,15 @@ class EmailUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.toString()
+
+
+
+@receiver(post_delete, sender=EmailUser)
+def delete_profile_for_user(sender, **kwargs):
+    related_profile_id = kwargs['instance'].profile_id
+
+    related_profile = UserProfile.objects.get(
+            id=related_profile_id
+    )
+
+    related_profile.delete()
