a3ad9c748daaeafd2f24b4e44fa3080a018506a4
Daniel Miranda <manieldiranda@gmail.com>
2022-04-07 17:28:00 -0700

    Update apps.py

--- pulseapi/users/apps.py
+++ pulseapi/users/apps.py
@@ -7,4 +7,6 @@ class UsersConfig(AppConfig):
     name = 'pulseapi.users'
 
     def ready(self):
+        # Importing this file so the post_delete signal in signals.py works.
+        # The noqa is added as without it, tests fail regarding a 'unused import'.
         import pulseapi.users.signals  # noqa: F401
